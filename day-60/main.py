a117351c28e5b6574ce1078dd83d20ac485a3c72
fkumsuz <kumsuzfurkan@gmail.com>
2024-05-16 23:05:02 +0300

    day60 basic forum added

---
+++ day-60/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html" )
+
+
+if __name__ == "__main__":
+    app.run(debug=True )
