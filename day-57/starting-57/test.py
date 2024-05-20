ab045dbf92fc87fc6592cee220a3314b3ac3da5c
fkumsuz <kumsuzfurkan@gmail.com>
2024-05-20 23:13:57 +0300

    updated

--- day-57/starting-57/test.py
+++ day-57/starting-57/test.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 import random
 import datetime
 import requests
+from post import Post
 
-blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-response = requests.get(blog_url)
-all_posts = response.json()
-
-for blog_post in all_posts:
-    print(blog_post )
-
- 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+for i in post_objects:
+    print(i.title)
